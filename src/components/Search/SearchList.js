0c60d76664ac0f7b93dba7553ae58c41dc12dac5
Danny Rangel <danielrangel.tech@gmail.com>
2019-04-28 19:38:45 -0700

    no results error checking

--- src/components/Search/SearchList.js
+++ src/components/Search/SearchList.js
@@ -2,11 +2,24 @@ import React from 'react';
 import SearchItem from './SearchItem';
 
 const SearchList = ({ videos, onVideoSelect, fontSize }) => {
+
+    
+
     const renderedList = videos.map((video, index) => {
         return <SearchItem key={index} onVideoSelect={onVideoSelect} video={video} fontSize={fontSize} />;
     });
 
-    return <>{renderedList}</>
+    if (videos.length === 0) {
+        return (
+            <div style={{margin: '100px auto', textAlign: 'center'}}>
+                <h2>No results found</h2>
+            </div>
+        );
+    } else {
+        return <>{renderedList}</>;
+    }
+
+    
 }
 
 export default SearchList;
